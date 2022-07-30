cae04ef54780464f7d443b01953093db20a3343f
Jamie Scott <git@jami.org.uk>
2022-07-30 22:53:50 +0100

    Start of decoding commands

--- OBSCaster/NewtekMiniController.cs
+++ OBSCaster/NewtekMiniController.cs
@@ -37,9 +37,39 @@ namespace OBSCaster {
         private void dataReceivedHandler(object sender, SerialDataReceivedEventArgs e) {
             SerialPort sp = (SerialPort)sender;
             string data = sp.ReadLine();
-            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(data);
-            var hexString = BitConverter.ToString(bytes);
-            Console.WriteLine($"Data received: {hexString}");
+            // TODO: need to check when data recv handler gets called - we may need to call multiple ReadLine()
+            // while bytestoread > 0 do a ReadLine
+            // byte[] bytes = System.Text.Encoding.UTF8.GetBytes(data);
+            // var hexString = BitConverter.ToString(bytes);
+            Console.WriteLine($"Data received: {data}");
+            this.decodeCommand(data);
+        }
+
+        private void decodeCommand(string command) {
+            if (command.Length != 4) {
+                Console.WriteLine($"Invalid command from RS-8: {command}");
+                return;
+            }
+            int bank = int.Parse(command.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+            int values = int.Parse(command.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+            //Console.WriteLine($"Bank: {bank}, Values: {values}");
+            if (bank >= 16 && bank <= 23) {
+                Console.WriteLine("Button");
+                return;
+            }
+            if (bank == 24) {
+                Console.WriteLine("Right knob");
+                return;
+            }
+            if (bank == 25) {
+                Console.WriteLine("Left knob");
+                return;
+            }
+            if (bank == 128) {
+                Console.WriteLine("TBar");
+                return;
+            }
+            Console.WriteLine("UNKNOWN BANK!");
         }
     }
 }
